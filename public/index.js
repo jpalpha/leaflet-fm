28f1f068f52d7231515e750eb198978ceff42c68
jpalpha <jean-philippe.palha@mediactivegroup.com>
2022-04-04 15:58:31 +0200

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,49 @@
+// Leaflet integration
+
+var tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    minZoom: 5,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }),
+  latlng = L.latLng(46.90296, 1.90925);
+
+var map = L.map("map", { center: latlng, zoom: 6, layers: [tiles] });
+
+var markersCluster = new L.MarkerClusterGroup();
+
+for (var i = 0; i < geojson.length; i++) {
+  var latLng = new L.LatLng(
+    geojson[i].geometry.coordinates[1],
+    geojson[i].geometry.coordinates[0]
+  );
+
+  var myID = geojson[i].recordid;
+  var myTitle = geojson[i].fields.titre;
+  var myIcon = L.icon({
+    iconUrl:
+      "https://fms19-dev.imadiff.net/" +
+      getIcon(
+        geojson[i].fields.categorie1,
+        geojson[i].fields.categorie2,
+        geojson[i].fields.categorie3
+      ),
+    iconSize: [33, 44],
+    iconAnchor: [16, 44],
+    popupAnchor: [-3, -76]
+  });
+
+  var marker = new L.Marker(latLng, { icon: myIcon })
+    .bindTooltip(myTitle)
+    .on("click", onMapClick.bind(myID));
+
+  markersCluster.addLayer(marker);
+}
+
+map.addLayer(markersCluster);
+
+function onMapClick(e) {
+  typeof FileMaker !== "undefined"
+    ? FileMaker.PerformScriptWithOption("FromJS", JSON.stringify(this), 0)
+    : console.log(this);
+}
